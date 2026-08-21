537ec33dc8dd6a2d85994a83e473ae34d24bcfe0
simmule-turner <31528536+simmule-turner@users.noreply.github.com>
2026-08-20 20:26:31 -0400

    Update CACHE_NAME to version 4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dc-calc-v3';
+const CACHE_NAME = 'dc-calc-v4';
 self.addEventListener('install', e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.add(self.registration.scope)));
